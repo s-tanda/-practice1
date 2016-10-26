95661d02c91ff1649234b0587e7b916658c49c98
s_tanda <s_tanda@softem-net.co.jp>
2016-10-26 18:55:06 +0900

    コンストラクタの追加

--- src/practice1_1/TestModel.java
+++ src/practice1_1/TestModel.java
@@ -1,6 +1,11 @@
 package practice1_1;
 
 public class TestModel {
+	public TestModel(String name) {
+		super();
+		this.name = name;
+	}
+	/** 名称 */
 	String name = "";
 
 	public String getName() {
