54b1ede767344730c9baa9a6386e3bb46eb0c894
s_tanda <s_tanda@softem-net.co.jp>
2016-10-26 17:51:35 +0900

    ｃを追加

--- src/practice1_1/Main.java
+++ src/practice1_1/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("最初に追加");
+
+
 		int a = 210;
 		int b = 200;
-		System.out.println(a+b);
+		int c = 200;
+		System.out.println(a+b+c);
 
 		System.out.println("最後に追加");
 	}
