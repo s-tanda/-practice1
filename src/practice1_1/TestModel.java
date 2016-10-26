10b48fdf732cdff087c8efb2ab0c4eba005fdb76
s_tanda <s_tanda@softem-net.co.jp>
2016-10-26 18:53:13 +0900

    モデルの追加

---
+++ src/practice1_1/TestModel.java
@@ -0,0 +1,13 @@
+package practice1_1;
+
+public class TestModel {
+	String name = "";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
