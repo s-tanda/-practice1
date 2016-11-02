378cd3d76f61886d05b2cf52cb654b2641786dcc
s_tanda <s_tanda@softem-net.co.jp>
2016-11-02 11:36:18 +0900

    ローカル変更

--- src/practice1_1/Util.java
+++ src/practice1_1/Util.java
@@ -25,7 +25,6 @@ public class Util {
 	 * @return
 	 */
 	public static double dAdd(double d1, double d2){
-		//コメント追加
 		return BigDecimal.valueOf(d1).add(BigDecimal.valueOf(d2)).doubleValue();
 	}
 }
