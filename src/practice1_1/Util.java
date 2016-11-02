326d4f53312289d5c080026a27cd0a6f47417684
s_tanda <s_tanda@softem-net.co.jp>
2016-11-02 10:16:40 +0900

    ユーティリティークラスを作成

---
+++ src/practice1_1/Util.java
@@ -0,0 +1,17 @@
+package practice1_1;
+
+/**
+ * ユーティリティクラス
+ * @author softem-n13
+ *
+ */
+public class Util {
+	/**
+	 * 引数の文字にhogeを追加して返す
+	 * @param arg
+	 * @return
+	 */
+	public static String addHoge(String arg){
+		return arg + "hoge";
+	}
+}
